60c81ca9fb3f629776304a3081edde6fe44d1c0b
Joel Griffith <joel@joel-griffith.com>
2011-11-24 22:52:28 -0500

    licenses, how do they work

--- muuselib/gui/listbox.py
+++ muuselib/gui/listbox.py
@@ -1,3 +1,22 @@
+# Muuse - Yet another audio player
+#
+# Copyright (c) 2011 Joel Griffith
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Library General Public
+# License as published by the Free Software Foundation; either
+# version 2 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Library General Public License for more details.
+#
+# You should have received a copy of the GNU Library General Public
+# License along with this library; if not, write to the
+# Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+# Boston, MA 02111-1307, USA.
+
 import gobject
 import pygtk
 pygtk.require('2.0')
